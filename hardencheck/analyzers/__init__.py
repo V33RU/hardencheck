0f684b2342b9543c358586871d3a52892740d672
`Mr-IoT` <veeru.rockstar249@gmail.com>
2026-03-11 01:48:39 +0530

    Create __init__.py

---
+++ hardencheck/analyzers/__init__.py
@@ -0,0 +1,39 @@
+"""Analyzer modules for firmware security analysis."""
+
+from hardencheck.analyzers.file_discovery import FileDiscovery
+from hardencheck.analyzers.firmware_profile import FirmwareProfiler
+from hardencheck.analyzers.binary_analysis import BinaryAnalyzer
+from hardencheck.analyzers.aslr_entropy import ASLREntropyAnalyzer
+from hardencheck.analyzers.daemon_detection import DaemonDetector
+from hardencheck.analyzers.banned_functions import BannedFunctionScanner
+from hardencheck.analyzers.credential_scanner import CredentialScanner
+from hardencheck.analyzers.certificate_scanner import CertificateScanner
+from hardencheck.analyzers.config_scanner import ConfigScanner
+from hardencheck.analyzers.security_testing import SecurityTester
+from hardencheck.analyzers.crypto_binary import CryptoBinaryDetector
+from hardencheck.analyzers.firmware_signing import FirmwareSigningAnalyzer
+from hardencheck.analyzers.service_privileges import ServicePrivilegeAnalyzer
+from hardencheck.analyzers.kernel_hardening import KernelHardeningAnalyzer
+from hardencheck.analyzers.update_mechanism import UpdateMechanismAnalyzer
+from hardencheck.analyzers.aslr_summary import ASLRSummaryGenerator
+from hardencheck.analyzers.sbom_generator import SBOMGenerator
+
+__all__ = [
+    "FileDiscovery",
+    "FirmwareProfiler",
+    "BinaryAnalyzer",
+    "ASLREntropyAnalyzer",
+    "DaemonDetector",
+    "BannedFunctionScanner",
+    "CredentialScanner",
+    "CertificateScanner",
+    "ConfigScanner",
+    "SecurityTester",
+    "CryptoBinaryDetector",
+    "FirmwareSigningAnalyzer",
+    "ServicePrivilegeAnalyzer",
+    "KernelHardeningAnalyzer",
+    "UpdateMechanismAnalyzer",
+    "ASLRSummaryGenerator",
+    "SBOMGenerator",
+]
